84e2057bd446fbf2e8127092bd0e789b0a5a3a73
Tharshini2006 <tharshinimurugan22@gmail.com>
2025-04-10 23:09:08 +0530

    Success

--- web.py
+++ web.py
@@ -1,23 +1,47 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 content = """
 <html>
-<head>
-<h2 style="font-family: Arial, sans-serif; color: blue; text-align: center;"><b>LIST OF PROTOCOLS</b></h1>
-        <h1>NAME:THARSHINI.M <br> 
-     REF NO:212224230287</h2>
+<h1 align="center",color="red"><u>
+     LIST OF PROTOCOLS
+</h1> </u>
+<h3 align="center">NAME:THARSHINI M</h3>
+<h3 align="center">REF NO:212224230287</h3>
+<b><u><head>
+     APPLICATION LAYER
+</b></u></head> 
+<ul>
+<li>HTTP-Hypertext Transfer Protocol</li>
+<li>FTP-File Tramsfer Protocol</li>
+<li>SMTP-Simple Mail Transfer Protocol</li>
+<li>SNMP-Simple Network Management Protocol</li>
+<li>DNS-Domain Name System</li>
+<li>Telnet-Telecommunications Network</li>
+</ul>
 
-    <title>TCP/IP Protocol Suite</title>
-</head>
-<body>
-    <h1>TCP/IP Protocol Suite</h1>
-    <ul>
-        <li>HTTP</li>
-        <li>FTP</li>
-        <li>SMTP</li>
-        <li>DNS</li>
-        <li>Telnet</li>
-        <li>SNMP</li>
-    </ul>
+<b><u><head>
+     TRANSPORT LAYER
+</b></u></head> 
+<ul>
+<li>TCP - Tramsmission Control Protocol</li>
+<li>UDP - User Datagram Peotocal</li>
+</ul>
+<b><u><head>
+     INTERNET LAYER
+</b></u></head> 
+<ul>
+<li>ICMP - Internet Control Message Protocol</li>
+<li>IGMP - Intenet Group Management Protocol</li>
+<li>IPV4 - Intenet Protocol Version4</li>
+<li>IPV6 - Intenet Protocol Version6</li>
+</ul>
+<b><u><head>
+     NETWORK ACEESS LAYER
+</b></u></head> 
+<ul>
+<li> MAC /Ethernet</li>
+<li>FDDI</li>
+<li>Frame Relay</li>
+</ul>
 </body>
 </html>
 """
